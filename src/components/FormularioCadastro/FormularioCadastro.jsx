b301ca1a53d6905ea48cdab251c4d0b0d7da172c
Guilherme Teixeira <guiemi.teixeira@gmail.com>
2020-11-04 14:27:59 -0300

    WIP

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,15 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from "./DadosUsuario";
 import DadosEntrega from "./DadosEntrega";
 
 function FormularioCadastro({ aoEnviar, validarCPF }) {
   const [etapaAtual, setEtapaAtual] = useState(0);
+  const [dadosColetados, setDadosColetados] = useState({})
+
+  useEffect(() => {
+    console.log(dadosColetados)
+  })
+
   const formularios = [
-    <DadosUsuario aoEnviar={proximo} />,
-    <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF} />,
-    <DadosEntrega aoEnviar={aoEnviar} />,
+    <DadosUsuario aoEnviar={coletarDados} />,
+    <DadosPessoais aoEnviar={coletarDados} validarCPF={validarCPF} />,
+    <DadosEntrega aoEnviar={coletarDados} />,
   ]
+
+  function coletarDados(dados) {
+    setDadosColetados({ ...dadosColetados, ...dados });
+    proximo();
+  }
 
   function proximo() {
     setEtapaAtual(etapaAtual + 1);
